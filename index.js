840f287ef43e42ba031dc01e6fe762b00a424448
Егор Вершков <vershkov.e@volgablob.ru>
2022-07-01 21:11:50 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
 import fs from "fs";
 import os from "os";
-import { env } from "node:process";
+import path from "path";
+import ObjectsToCsv from "objects-to-csv";
+import dotenv from "dotenv";
+import { humanFileSize } from "./utils";
 
 // ---- Settings ---- //
-const scanFolder = os.homedir();
-const excludeFolders = ["node_modules"];
-const excludeExtensions = [];
-const excludeFiles = [".DS_Store"];
-// ---- Settings ---- //
-
-/**
- * Format bytes as human-readable text.
- *
- * @param bytes Number of bytes.
- * @param si True to use metric (SI) units, aka powers of 1000. False to use
- *           binary (IEC), aka powers of 1024.
- * @param dp Number of decimal places to display.
- *
- * @return Formatted string.
- */
-function humanFileSize(bytes, si = false, dp = 1) {
-  const thresh = si ? 1000 : 1024;
-
-  if (Math.abs(bytes) < thresh) {
-    return bytes + " B";
-  }
-
-  const units = si
-    ? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-    : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
-  let u = -1;
-  const r = 10 ** dp;
-
-  do {
-    bytes /= thresh;
-    ++u;
-  } while (
-    Math.round(Math.abs(bytes) * r) / r >= thresh &&
-    u < units.length - 1
-  );
-
-  return bytes.toFixed(dp) + " " + units[u];
+dotenv.config();
+let scanFolder;
+if (process.env.APPEND_HOME_PATH === "true") {
+  scanFolder = os.homedir() + process.env.SCAN_FOLDER;
+} else {
+  scanFolder = process.env.SCAN_FOLDER;
 }
+
+const excludeFolders = ["node_modules", ".git"];
+const includeExtensions = [".js", ".jsx", ".ts", ".tsx", ".css"];
+const excludeFiles = [".DS_Store"];
+const hidePathString = process.env.HIDE_PATH_STRING;
+// ---- Settings ---- //
 
 // Scan folders
 const getAllFiles = function (dirPath) {
   var results = [];
   var list = fs.readdirSync(dirPath);
   list.forEach(function (file) {
-    file = dirPath + "/" + file;
-    var stat = fs.statSync(file);
-    if (stat && stat.isDirectory()) {
-      /* Recurse into a subdirectory */
-      results = results.concat(getAllFiles(file));
-    } else {
-      /* Is a file */
-      results.push(file + ` [${humanFileSize(stat.size)}]`);
+    if (!excludeFolders.includes(file) && !excludeFiles.includes(file)) {
+      file = dirPath + "/" + file;
+      var stat = fs.statSync(file);
+
+      if (stat && stat.isDirectory()) {
+        /* Recurse into a subdirectory */
+        results = results.concat(getAllFiles(file));
+      } else {
+        /* Is a file */
+        const ext = path.extname(file);
+        if (includeExtensions.includes(ext)) {
+          results.push({
+            path: file.replace(hidePathString, ""),
+            byte: stat.size,
+            humanSize: `${humanFileSize(stat.size)}`,
+          });
+        }
+      }
     }
   });
   return results;
@@ -63,7 +50,10 @@ const getAllFiles = function (dirPath) {
 
 try {
   const result = getAllFiles(scanFolder);
-  console.log(result.slice(0, 10));
+  const csv = new ObjectsToCsv(result);
+  await csv.toDisk("./reports/result.csv");
+
+  // console.log(result.slice(0, 10));
 } catch (e) {
   console.log(e);
 }
